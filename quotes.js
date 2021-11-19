5ea4aec3650ae53f75054d518c2bd64fa04fa91a
Flyspeck101 <yeetersdeleters12345@gmail.com>
2021-11-19 11:26:41 +1100

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,5 +1,19 @@
-const quotes = [
-  
-];
-
-document.getElementById("quote").innerText = quotes[(new Date()).getDate()];
+document.getElementById("quote").innerText = ([
+  "There is no destiny. There is only hope.", 
+  "The only monster of life is inside you.",
+  "The world is as you see it.",
+  "There is none that is impossible, only that which we do not dare.",
+  "The hardest battle to win is the battle within yourself.",
+  "Your life is one of a kind. Make it worthwhile.",
+  "If you always think about bad things, you'll notice them.",
+  "Do. Or do not. There is no try.",
+  "It doesn't matter what other people think of you, because you don't have to listen.",
+  "It doesn't mean anything to say you will. Just do it.",
+  "No man succeeded by giving up.",
+  "It doesn't matter what others think of you, only what others think of what you do.",
+  "Judge a book by its contents, not its reviews.",
+  "Don't compare yourself to others. They aren't you.",
+  "Your dreams are your own.",
+  "Don't disregard the advice of others. Try to understand them.",
+  "
+])[(new Date()).getDate() - 1];
